2021cb7269722e7521420f3bfd7ecf5ace0f5d12
rishavcool21 <kumarrishavcool21@gmail.com>
2023-10-05 09:40:00 +0530

    Lifted stateUp

--- S8_project/src/components/UserInput/UserInput.js
+++ S8_project/src/components/UserInput/UserInput.js
@@ -7,13 +7,14 @@ const initialUserInput ={
     duration: 10
 }
 
-const UserInput = () => {
+const UserInput = (props) => {
   
   const [userInput, setUserInput] = useState(initialUserInput)
 
   const submitHandler = (event) => {
     event.preventDefault();
     console.log("Submit");
+    props.onCalculate(userInput);
   };
 
   const resetHandler = () => {
