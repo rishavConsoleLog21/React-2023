1e7b0bf88bfd232c6b01c20dfd56dba3ceb99322
rishavcool21 <kumarrishavcool21@gmail.com>
2023-10-07 14:17:35 +0530

    Added Portals

--- S8_project2/src/App.js
+++ S8_project2/src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import AddUser from "./components/Users/AddUser";
 import UserList from "./components/Users/UserList";
+
 function App() {
   const [usersList, setUsersList] = useState([]);
 
@@ -14,10 +15,10 @@ function App() {
   };
 
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UserList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
