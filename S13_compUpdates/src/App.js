0135995591cb97ba9f39294fad9092eb4c726488
rishavcool21 <kumarrishavcool21@gmail.com>
2023-10-22 12:18:06 +0530

    Added Toggle Para Btn

--- S13_compUpdates/src/App.js
+++ S13_compUpdates/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button";
+import "./App.css";
 
 function App() {
+  const [showPara,setShowPara] = useState(false);
+
+  const toggleParaHandler = () => {
+    setShowPara(prevShowPara => !prevShowPara);
+  }
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showPara && <p>This is new!</p>}
+      <Button onClick={() => toggleParaHandler(!showPara)}>Toggle Paragraph!</Button>
     </div>
   );
 }
