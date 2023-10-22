8f58665ef11b8c4f402fbc6eadc5a74e5452d113
rishavcool21 <kumarrishavcool21@gmail.com>
2023-10-22 23:35:34 +0530

    Added Allow Toggle button

--- S13_compUpdates/src/App.js
+++ S13_compUpdates/src/App.js
@@ -1,19 +1,31 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
+  
   const [showPara, setShowPara] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+  
   console.log("APP RUNNING");
-  const toggleParaHandler = () => {
+  
+  const toggleParaHandler = useCallback(() => {
+    if (allowToggle){
+      setShowPara((prevShowPara) => !prevShowPara);
+    }
     setShowPara((prevShowPara) => !prevShowPara);
-  };
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  }
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showPara} />
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
       <Button onClick={toggleParaHandler}>Toggle Paragraph!</Button>
     </div>
   );
