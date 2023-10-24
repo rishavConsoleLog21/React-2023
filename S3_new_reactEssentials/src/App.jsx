98a226abb8cf914e04ec3e160f937e8e4f6f978b
rishavcool21 <kumarrishavcool21@gmail.com>
2023-10-24 22:30:27 +0530

    Updated S3_new

--- S3_new_reactEssentials/src/App.jsx
+++ S3_new_reactEssentials/src/App.jsx
@@ -1,16 +1,40 @@
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
+
 function App() {
+  function handleSelect() {
+    console.log("Tab selected");
+  }
+
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((coreConcept) => (
+              <CoreConcept
+                key={coreConcept.id}
+                image={coreConcept.image}
+                title={coreConcept.title}
+                description={coreConcept.description}
+              />
+            ))}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Example</h2>
+          <menu>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
+          </menu>
+          Dynamic Content
+        </section>
       </main>
     </div>
   );
---
+++ S3_new_reactEssentials/src/components/TabButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TabButton = ({ children, onSelect }) => {
+  return (
+    <li>
+      <button onClick={onSelect}>{children}</button>
+    </li>
+  );
+};
+
+export default TabButton;
