17c82c98a2c700719a105a18e38de0c670ccb717
rishavcool21 <kumarrishavcool21@gmail.com>
2023-11-03 23:24:19 +0530

    Update styling and background image

--- S6_new_stylingReact/src/components/Button.jsx
+++ S6_new_stylingReact/src/components/Button.jsx
@@ -1,18 +1,7 @@
-import { styled } from 'styled-components';
+import React from 'react'
 
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
+function Button({children, ...props}) {
+  return <button className="px-4 py-2 font-semibold uppercase rounded text-stone-900 bg-green-400 hover:bg-green-500" {...props}>{children}</button>
+}
 
-  &:hover {
-    background-color: #f0920e;
-  }
-`;
-
-export default Button;
+export default Button
--- S6_new_stylingReact/src/components/Header.jsx
+++ S6_new_stylingReact/src/components/Header.jsx
@@ -8,8 +8,8 @@ export default function Header() {
         alt="A canvas"
         className="mb-8 w-44 h-44 object-contain"
       />
-      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-blue-800 font-title">ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <h1 className="text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-blue-600 font-title">ReactArt</h1>
+      <p className="text-stone-600">A community of artists and art-lovers.</p>
     </header>
   );
 }
