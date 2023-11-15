65758cd29d7b45a020d98cec7037ffecd28215f4
rishavcool21 <kumarrishavcool21@gmail.com>
2023-11-15 07:18:15 +0530

    Refactor cart functionality and update context API

--- S10_contextAPI_useReducer/src/store/CartContext.jsx
+++ S10_contextAPI_useReducer/src/store/CartContext.jsx
@@ -3,6 +3,7 @@ import {createContext} from 'react'
 const CartContext = createContext({
     items: [],
     addItemToCart: () => {},
+    updateItemQuantity: () => {}
 })
 
 export default CartContext;
