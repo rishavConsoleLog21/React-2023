250a0353f5583ae613637368bc10ee4abc9f2a11
rishavcool21 <kumarrishavcool21@gmail.com>
2023-11-18 00:08:09 +0530

    Refactor pickedPlaces state initialization and update logic

--- S11_useEffect/src/App.jsx
+++ S11_useEffect/src/App.jsx
@@ -7,11 +7,16 @@ import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
 import logoImg from "./assets/logo.png";
 import { sortPlacesByDistance } from "./loc.js";
 
+const storeIds = JSON.parse(localStorage.getItem("selectPlaces")) || [];
+const storedPlaces = storeIds.map((id) =>
+  AVAILABLE_PLACES.find((place) => place.id === id)
+);
+
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -41,7 +46,7 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
-    
+
     const storeIds = JSON.parse(localStorage.getItem("selectPlaces")) || [];
     if (storeIds.indexOf(id) === -1) {
       localStorage.setItem("selectPlaces", JSON.stringify([id, ...storeIds])); //localStorage is a browser built-in feature
@@ -53,6 +58,12 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storeIds = JSON.parse(localStorage.getItem("selectPlaces")) || [];
+    localStorage.setItem(
+      "selectPlaces",
+      JSON.stringify(storeIds.filter((id) => id !== selectedPlace.current))
+    );
   }
 
   return (
