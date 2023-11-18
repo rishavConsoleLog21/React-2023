db56a34b1c336c4a8906aa55bd82b9ce285f1bc2
rishavcool21 <kumarrishavcool21@gmail.com>
2023-11-18 23:56:01 +0530

    Refactor Modal component to use useEffect hook

--- S11_useEffect/src/components/Modal.jsx
+++ S11_useEffect/src/components/Modal.jsx
@@ -1,14 +1,22 @@
-import { useRef } from 'react';
-import { createPortal } from 'react-dom';
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
 
-function Modal({open, children }) {
+function Modal({ open, children }) {
   const dialog = useRef();
+
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal(); // showModal() is a browser built-in feature to show backdrop
+    } else {
+      dialog.current.close();
+    }
+  }, []);
 
   return createPortal(
     <dialog className="modal" ref={dialog} open={open}>
       {children}
     </dialog>,
-    document.getElementById('modal')
+    document.getElementById("modal")
   );
 }
 
