f3b8a42c0c786f3b9964c94aa76ce94b7d09070e
rishavcool21 <kumarrishavcool21@gmail.com>
2023-11-20 00:02:05 +0530

    Fixed onClose Error

--- S11_useEffect/src/components/Modal.jsx
+++ S11_useEffect/src/components/Modal.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-function Modal({ open, children,onClose }) {
+function Modal({ open, children, onClose }) {
   const dialog = useRef();
 
   useEffect(() => {
@@ -13,7 +13,7 @@ function Modal({ open, children,onClose }) {
   }, [open]);
 
   return createPortal(
-    <dialog className="modal" ref={dialog} open={open} onClose={onClose}>
+    <dialog className="modal" ref={dialog} onClose={onClose}>
       {children}
     </dialog>,
     document.getElementById("modal")
