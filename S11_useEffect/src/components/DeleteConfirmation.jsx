6fab641ef6220b6d8c8ba75e0ed8c69637aa6e71
rishavcool21 <kumarrishavcool21@gmail.com>
2023-11-20 12:32:14 +0530

    Added useEffect to DeleteConfirmation

--- S11_useEffect/src/components/DeleteConfirmation.jsx
+++ S11_useEffect/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    console.log("Timer Set");
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, 3000);
+
+    return () => {
+      console.log("Timer Cleared");
+      clearTimeout(timer);
+    };
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
