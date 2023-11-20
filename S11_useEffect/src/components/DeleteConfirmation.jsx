253dc17e85082f9373597872814cf1398c14c138
rishavcool21 <kumarrishavcool21@gmail.com>
2023-11-20 13:46:46 +0530

    Added timer functionality to DeleteConfirmation

--- S11_useEffect/src/components/DeleteConfirmation.jsx
+++ S11_useEffect/src/components/DeleteConfirmation.jsx
@@ -1,11 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime, setRemainingTime] = useState(TIMER);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 20);
+    }, 20);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   useEffect(() => {
     console.log("Timer Set");
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMER);
 
     return () => {
       console.log("Timer Cleared");
@@ -25,6 +38,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMER} />
     </div>
   );
 }
