f20e5c8eaf7e50ca01f6849349daa1e073996f7b
rishavcool21 <kumarrishavcool21@gmail.com>
2023-11-20 12:44:41 +0530

    Added useCallback to handleRemovePlace function

--- S11_useEffect/src/App.jsx
+++ S11_useEffect/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
@@ -53,7 +53,7 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -64,7 +64,7 @@ function App() {
       "selectPlaces",
       JSON.stringify(storeIds.filter((id) => id !== selectedPlace.current))
     );
-  }
+  }, []);
 
   return (
     <>
