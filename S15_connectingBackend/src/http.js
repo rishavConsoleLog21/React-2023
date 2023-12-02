3e30f2481d62cc46b24b432cbffe9a5bd5e92079
rishavcool21 <kumarrishavcool21@gmail.com>
2023-12-02 22:58:08 +0530

    Add updateUserPlaces function and handleSelectPlace async

--- S15_connectingBackend/src/http.js
+++ S15_connectingBackend/src/http.js
@@ -7,3 +7,21 @@ export async function fetchAvailablePlaces() {
 
   return resData.places;
 }
+
+export async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({
+      places: places
+    }), // we can use ({places}) because we have a key and value with the same name
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  const resData = await response.json();
+
+  if(!response.ok) {
+    throw new Error("Could not update user places")
+  }
+  return resData.message;
+}
