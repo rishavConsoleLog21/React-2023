2fb1fd1b37f4e8f8c7644d493cd74fab496881ab
rishavcool21 <kumarrishavcool21@gmail.com>
2023-12-04 19:47:32 +0530

    email validation with lostFocus(blur)

--- S17_forms/src/components/StateLogin.jsx
+++ S17_forms/src/components/StateLogin.jsx
@@ -8,6 +8,17 @@ export default function Login() {
     password: "",
   });
 
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  //code for validating email
+  const emailIsValid = didEdit.email && !enteredInput.email.includes("@");
+
+  //code for validating password with at least 8 characters long, no spaces, at least one uppercase letter, one lowercase letter, one number and one special character
+  //const passwordIsValid = 
+
   function handleSubmit(e) {
     e.preventDefault();
     console.log(enteredInput);
@@ -20,11 +31,22 @@ export default function Login() {
   //   setEnteredPassword(e.target.value);
   // }
 
-  function handleInputChange(identifier, value){
-    setEnteredInput(prevValue => ({
+  function handleInputChange(identifier, value) {
+    setEnteredInput((prevValue) => ({
       ...prevValue,
-      [identifier]: value
-    }))
+      [identifier]: value,
+    }));
+    setDidEdit((prevValue) => ({
+      ...prevValue,
+      [identifier]: false,
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit((prevValue) => ({
+      ...prevValue,
+      [identifier]: true,
+    }));
   }
 
   return (
@@ -38,9 +60,13 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur("email")}
             onChange={(e) => handleInputChange("email", e.target.value)}
             value={enteredInput.email}
           />
+        </div>
+        <div className="control-error">
+          {emailIsValid && <p>Please enter a valid email address.</p>}
         </div>
 
         <div className="control no-margin">
@@ -49,9 +75,19 @@ export default function Login() {
             id="password"
             type="password"
             name="password"
+            // onBlur={() => handleInputBlur("password")}
             onChange={(e) => handleInputChange("password", e.target.value)}
             value={enteredInput.password}
           />
+        </div>
+        <div className="control-error">
+          {passwordIsValid && (
+            <p>
+              password with at least 8 characters long, no spaces, at least 1
+              uppercase letter, 1 lowercase letter, 1 number and 1 special
+              character
+            </p>
+          )}
         </div>
       </div>
 
