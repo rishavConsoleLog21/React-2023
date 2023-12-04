a8695cc64917fcd4e86df20f77cd8babba6283b5
rishavcool21 <kumarrishavcool21@gmail.com>
2023-12-04 13:06:52 +0530

    Refactor Login, to use 1 useState for inputChange

--- S17_forms/src/components/Login.jsx
+++ S17_forms/src/components/Login.jsx
@@ -1,8 +1,30 @@
+import React, { useState } from "react";
+
 export default function Login() {
-  
-  function handleSubmit(e){
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredInput, setEnteredInput] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(e) {
     e.preventDefault();
-    console.log('Login form submitted');
+    console.log(enteredInput);
+  }
+
+  // function handleEmailChange(e) {
+  //   setEnteredEmail(e.target.value);
+  // }
+  // function handlePasswordChange(e) {
+  //   setEnteredPassword(e.target.value);
+  // }
+
+  function handleInputChange(identifier, value){
+    setEnteredInput(prevValue => ({
+      ...prevValue,
+      [identifier]: value
+    }))
   }
 
   return (
@@ -12,12 +34,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(e) => handleInputChange("email", e.target.value)}
+            value={enteredInput.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(e) => handleInputChange("password", e.target.value)}
+            value={enteredInput.password}
+          />
         </div>
       </div>
 
