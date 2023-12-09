2e61367dc7ec1717fd44e59968438e46b96d676b
rishavcool21 <kumarrishavcool21@gmail.com>
2023-12-09 23:50:52 +0530

    redux basic example

--- S19_redux/reduxDemo.js
+++ S19_redux/reduxDemo.js
@@ -1,5 +1,19 @@
-const redux = require('redux');
+const redux = require("redux");
 
-const counterReducer = () => {};
+const counterReducer = (state = { counter: 0 }, action) => {
+  return {
+    counter: state.counter + 1,
+  };
+};
 
-const store = redux.createStore()
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
+
