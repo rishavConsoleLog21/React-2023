8a94038a7948ca4c1cbd32aad731d626439a582e
rishavcool21 <kumarrishavcool21@gmail.com>
2023-12-11 15:08:03 +0530

    + implement toggleCounterHandler

--- S19_reduxProject/src/components/Counter.js
+++ S19_reduxProject/src/components/Counter.js
@@ -4,25 +4,28 @@ import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
   };
 
   const increaceHandler = () => {
-    dispatch({type: 'increase', amount: 10})
-  }
+    dispatch({ type: "increase", amount: 10 });
+  };
 
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaceHandler}>Increment + 10</button>
