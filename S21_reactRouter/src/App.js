7ab56dae3729b80727e77d820040fcb5657b65d5
rishavcool21 <kumarrishavcool21@gmail.com>
2023-12-16 00:34:18 +0530

    Add routes for ProductPages in App.js & Alter way to create routes

--- S21_reactRouter/src/App.js
+++ S21_reactRouter/src/App.js
@@ -1,10 +1,20 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
+import ProductPages from "./pages/Products";
 
 const router = createBrowserRouter([
-  {path: '/', element: <HomePage />},
-  {}
-])
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductPages /> },
+]);
+
+// Alternative way to create routes
+// const routeDefinitions = createRoutesFromElements(
+//   <>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductPages />} />
+//   </>
+// );
+// const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
---
+++ S21_reactRouter/src/pages/Products.js
@@ -0,0 +1,11 @@
+//Add code for Product page here
+function ProductPages() {
+  return (
+    <div>
+      <h1>Product</h1>
+      <p>Product page content</p>
+    </div>
+  );
+}
+
+export default ProductPages;
