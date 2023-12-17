86ff4e34c34c9be89d1c8ad0936b3e2ad8386a0e
rishavcool21 <kumarrishavcool21@gmail.com>
2023-12-17 21:01:38 +0530

    Refactor Error and EventsPage components

--- S21_reactRouteProject/frontend/src/pages/Error.js
+++ S21_reactRouteProject/frontend/src/pages/Error.js
@@ -1,13 +1,29 @@
-import React from "react";
+import { useRouteError } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
+import PageContent from "../components/PageContent";
 
 const Error = () => {
+  const error = useRouteError();
+
+  let title = "An error occurred!";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Page not found!";
+    message = "The page you are looking for does not exist!";
+  }
+
   return (
     <>
       <MainNavigation />
       <main>
-        <h1>404 Page Not Found</h1>
-        <p>Sorry, the page you are looking for does not exist.</p>
+        <PageContent title={title}>
+          <p>{message}</p>
+        </PageContent>
       </main>
     </>
   );
--- S21_reactRouteProject/frontend/src/pages/EventsPage.js
+++ S21_reactRouteProject/frontend/src/pages/EventsPage.js
@@ -14,7 +14,9 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    throw new Error("Something went wrong!");
+    throw new Response(JSON.stringify({ message: "Failed to load events!" }), {
+      status: 500,
+    });
   } else {
     return response;
   }
