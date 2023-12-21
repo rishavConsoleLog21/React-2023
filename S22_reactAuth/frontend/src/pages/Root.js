ba4c55825b3ecc116dd211225a1d5e3aa14582cc
rishavcool21 <kumarrishavcool21@gmail.com>
2023-12-21 17:53:50 +0530

    Add token expiration functionality

--- S22_reactAuth/frontend/src/pages/Root.js
+++ S22_reactAuth/frontend/src/pages/Root.js
@@ -1,10 +1,30 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
 
 function RootLayout() {
-  // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+  
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
 
+    if(token === "EXPIRED") {
+      localStorage.removeItem("token");
+      localStorage.removeItem("expiration");
+      return;
+    }
+
+    const tokenDuration = getTokenDuration()
+
+    setTimeout(() => {
+      submit(null, { method: "POST", action: "/logout" });
+    }, tokenDuration);
+  }, [token, submit]);
   return (
     <>
       <MainNavigation />
--- S22_reactAuth/frontend/src/util/auth.js
+++ S22_reactAuth/frontend/src/util/auth.js
@@ -1,8 +1,38 @@
+import { redirect } from "react-router-dom";
+
+export function getTokenDuration() {
+  const storeExpirationDate = localStorage.getItem("expiration");
+  const expirationDate = new Date(storeExpirationDate);
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getTime();
+  return duration;
+}
+
 export function getAuthToken() {
-    const token = localStorage.getItem("token");
-    return token;
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return null;
+  }
+
+  const tokenDuratoin = getTokenDuration();
+
+  if (tokenDuratoin <= 0) {
+    return "EXPIRED";
+  }
+
+  return token;
 }
 
 export function tokenLoader() {
-    return getAuthToken();
+  return getAuthToken();
+}
+
+export function checkToken() {
+  const token = getAuthToken();
+
+  if (!token) {
+    return redirect("/auth");
+  }
+  return null;
 }
