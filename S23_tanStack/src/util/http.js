46e9dda3ca69d04e11904541452d97fb0e56f000
rishavcool21 <kumarrishavcool21@gmail.com>
2023-12-23 12:46:23 +0530

    +crateEvent & selectImg from http & Refactor EventForm and NewEvent components

--- S23_tanStack/src/util/http.js
+++ S23_tanStack/src/util/http.js
@@ -18,3 +18,41 @@ export async function fetchEvents({ signal, searchTerm }) {
 
   return events;
 }
+
+export async function createNewEvent(eventData) {
+  const response = await fetch(`http://localhost:3000/events`, {
+    method: "{POST",
+    body: JSON.stringify(eventData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while creating the event");
+    error.info = await response.json();
+    error.code = response.status;
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
+
+export async function fetchSelectableImages({ signal }) {
+  const response = await fetch(`http://localhost:3000/events/images`, {
+    signal,
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the images");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { images } = await response.json();
+
+  return images;
+}
