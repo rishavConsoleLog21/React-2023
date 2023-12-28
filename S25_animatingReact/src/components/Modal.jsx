c0295a7cb5069d07ceea10d91fc62ac199f46fc4
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2023-12-28 15:05:13 +0530

    + animations for modal, button and more

--- S25_animatingReact/src/components/Modal.jsx
+++ S25_animatingReact/src/components/Modal.jsx
@@ -1,14 +1,23 @@
-import { createPortal } from 'react-dom';
+import { createPortal } from "react-dom";
+import { motion } from "framer-motion";
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <motion.dialog
+        
+        initial={{ opacity: 0, y: 30 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ type: "spring", stiffness: 260, damping: 20, duration: 0.5}}
+        exit={{ opacity: 0, y: 30 }}
+        open
+        className="modal"
+      >
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
-    document.getElementById('modal')
+    document.getElementById("modal")
   );
 }
