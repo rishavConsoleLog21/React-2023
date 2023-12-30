cd4f8c5eb3613e34e1b92f97a00b68069216f251
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2023-12-31 00:22:09 +0530

    + CSS classes

--- S26_typescriptReactProject/src/components/NewTodo.tsx
+++ S26_typescriptReactProject/src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import classes from "./NewTodo.module.css";
 
 const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
@@ -16,7 +17,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <label htmlFor="text"></label>
       <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
--- S26_typescriptReactProject/src/components/Todos.tsx
+++ S26_typescriptReactProject/src/components/Todos.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Todo from "../models/todo";
 import TodoItem from "./TodoItem";
+import classes from "./Todos.module.css";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
-    <ol>
+    <ol className={classes.todos}>
       {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text}/>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ol>
   );
