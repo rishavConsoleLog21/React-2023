68d0935e417f985daccba2f94d7f28817fda28ad
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2023-12-30 19:58:37 +0530

    Add NewTodo comp and implement addTodoHandler

--- S26_typescriptReactProject/src/App.tsx
+++ S26_typescriptReactProject/src/App.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
