6e02aea2358ab6e7aeacb347301d861720ba5c34
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2023-12-31 12:48:45 +0530

    Refactor App component and implement TodoContextProvider

--- S26_typescriptReactProject/src/App.tsx
+++ S26_typescriptReactProject/src/App.tsx
@@ -1,29 +1,14 @@
-import { useState } from "react";
 import Todos from "./components/Todos";
-import Todo from "./models/todo";
 import NewTodo from "./components/NewTodo";
+import TodoContextProvider from "./store/todo-context";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const addTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
-
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo);
-    });
-  };
-
-  const removeTodoHandler = (todoId: string) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.id !== todoId);
-    });
-  };
-
   return (
     <div>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
+      <TodoContextProvider>
+        <NewTodo />
+        <Todos />
+      </TodoContextProvider>
     </div>
   );
 }
